3df5d9c529cf81cadc569be934e62a5b07f83a35
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 19:01:23 +0200

    Aumneto de test para la clase C31

--- src/test/java/demo/control/versiones/C31Test.java
+++ src/test/java/demo/control/versiones/C31Test.java
@@ -1,5 +1,27 @@
 package demo.control.versiones;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class C31Test {
+    private C31 c31;
+
+    @Before
+    public void before() {
+        c31 = new C31();
+    }
+
+    @Test
+    public void testC11m1() {
+        assertEquals("C31.m1" ,c31.m1());
+    }
+    
+    @Test
+    public void testC11m2() {
+        assertEquals("C31.m2" ,c31.m2());
+    }
+
 
 }
