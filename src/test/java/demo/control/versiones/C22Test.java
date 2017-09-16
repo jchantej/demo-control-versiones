85cd55c2bd5ddafbc301e6ab0fb2b19f0e390288
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 18:55:13 +0200

    TEst empaauetado en desarrollo

--- src/test/java/demo/control/versiones/C22Test.java
+++ src/test/java/demo/control/versiones/C22Test.java
@@ -1,5 +1,21 @@
 package demo.control.versiones;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class C22Test {
+    private C22 c22;
+
+    @Before
+    public void before() {
+        c22 = new C22();
+    }
+
+    @Test
+    public void testC21m1() {
+        assertEquals("C22.mA" ,c22.mA());
+    }
 
 }
