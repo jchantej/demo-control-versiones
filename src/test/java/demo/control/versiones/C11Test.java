f5b8d9fe6343181f8e5a00ee0f12a9fa7d0284e7
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 16:29:44 +0200

    COnfiguraciones para pruebas de integracion Travis-Sonar

--- src/test/java/demo/control/versiones/C11Test.java
+++ src/test/java/demo/control/versiones/C11Test.java
@@ -1,5 +1,27 @@
 package demo.control.versiones;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class C11Test {
+    private C11 c11;
+
+    @Before
+    public void before() {
+        c11 = new C11();
+    }
+
+    @Test
+    public void testC11m1() {
+        assertEquals("C11.m1" ,c11.m1());
+    }
+    
+    @Test
+    public void testC11m2() {
+        assertEquals("C11.m2" ,c11.m2());
+    }
+
 
 }
--- src/test/java/demo/control/versiones/C21Test.java
+++ src/test/java/demo/control/versiones/C21Test.java
@@ -1,5 +1,27 @@
 package demo.control.versiones;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class C21Test {
+
+    private C21 c21;
+
+    @Before
+    public void before() {
+        c21 = new C21();
+    }
+
+    @Test
+    public void testC21m1() {
+        assertEquals("C21.m1" ,c21.m1());
+    }
+    
+    @Test
+    public void testC21m2() {
+        assertEquals("C21.m2" ,c21.m2());
+    }
 
 }
