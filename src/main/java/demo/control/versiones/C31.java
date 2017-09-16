7c874090feee1034d6d1eda9156dd28d2327af27
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 03:27:17 +0200

    Creación de la clase C31

---
+++ src/main/java/demo/control/versiones/C31.java
@@ -0,0 +1,12 @@
+package demo.control.versiones;
+
+public class C31 {
+    public String m1() {
+        return "C31.m1";
+    }
+    
+    public String m2() {
+        return "C31.m2";
+    }
+
+}
