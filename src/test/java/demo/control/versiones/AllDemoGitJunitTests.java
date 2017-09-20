630166e2737d89d4c023a2dcdf3cce1a515d56e2
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-20 15:53:34 +0200

    dsgsfgdfsg

---
+++ src/test/java/demo/control/versiones/AllDemoGitJunitTests.java
@@ -0,0 +1,20 @@
+package demo.control.versiones;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    C11Test.class,
+    C12Test.class,
+    C21Test.class,
+    C22Test.class,
+    C31Test.class,
+    C52Test.class
+})
+public class AllDemoGitJunitTests {
+
+}
---
+++ src/test/java/demo/control/versiones/C12Test.java
@@ -0,0 +1,14 @@
+package demo.control.versiones;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class C12Test {
+
+    @Test
+    public void test() {
+        assertEquals("C12.mA" , new C12().mA());
+    }
+
+}
